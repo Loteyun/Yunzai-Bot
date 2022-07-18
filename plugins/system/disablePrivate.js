29cc72c8d532322fcf50f0fea26abc0e27a94ddd
Le-niao <805475874@qq.com>
2022-07-18 09:41:34 +0800

    禁止私聊发送消息增加冷却cd

--- plugins/system/disablePrivate.js
+++ plugins/system/disablePrivate.js
@@ -6,9 +6,9 @@ export class disPri extends plugin {
     super({
       name: '禁止私聊',
       dsc: '对私聊禁用做处理当开启私聊禁用时只接收cookie以及抽卡链接',
-      event: 'message.private',
-      priority: 1
+      event: 'message.private'
     })
+    this.priority = 0
   }
 
   async accept () {
@@ -17,16 +17,31 @@ export class disPri extends plugin {
     if (this.e.isMaster) return
 
     /** 绑定ck，抽卡链接 */
-    if (!this.e.msg?.match(/(.*)(ltoken|_MHYUUID|authkey=)(.*)/g)) {
-      this.e.reply(cfg.other.disableMsg)
+    if (this.e.raw_message && !this.e.raw_message.match(/(.*)(ltoken|_MHYUUID|authkey=)(.*)/g)) {
+      this.sendTips()
       return 'return'
     }
 
     /** 发送日志文件，xlsx，json */
     if (this.e.file && !this.e.file?.name?.includes(['txt', 'xlsx', 'json'])) {
-      this.e.reply(cfg.other.disableMsg)
+      this.sendTips()
       return 'return'
     }
+  }
+
+  async sendTips () {
+    /** 冷却cd 10s */
+    let cd = 10
+
+    if (this.e.user_id == cfg.qq) return
+
+    /** cd */
+    let key = `Yz:disablePrivate:${this.e.user_id}`
+    if (await redis.get(key)) return
+
+    this.e.reply(cfg.other.disableMsg)
+
+    redis.setEx(key, cd, '1')
   }
 }
 
@@ -35,9 +50,9 @@ export class disFriPoke extends plugin {
     super({
       name: '禁止私聊',
       dsc: '对私聊禁用做处理当开启私聊禁用时只接收cookie以及抽卡链接',
-      event: 'notice.friend.poke',
-      priority: 1
+      event: 'notice.friend.poke'
     })
+    this.priority = 0
   }
 
   async accept () {
